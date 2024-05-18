781a59f74c4fcbcb8efc7515d9dd19d8f5de5420
Sidney Cadot <sidney@jigsaw.nl>
2024-05-18 21:29:24 +0200

    Improved comments.

--- source/usbtmc/__init__.py
+++ source/usbtmc/__init__.py
@@ -1,8 +1,8 @@
-"""The usbtmc package provides a cross-platform user-space USBTMC driver.
+"""The usbtmc package provides a cross-platform user-space driver for USBTMC-capable devices.
 
 This package implements the USBTMC protocol and the USBTMC-USB488 sub-protocol as described in [1] and [2].
 
-Some protocol features that are not very useful in most use-cases were omitted. In particular:
+The API presented does not support protocol features that are not very useful in most use-cases. Specifically:
 
 * For Bulk I/O, only device-dependent messages are supported. Support for vendor-specific messages is not implemented.
 * For Bulk I/O, the "TermChar" feature is not supported. This feature allows the Host to request a specific termination
@@ -15,8 +15,8 @@ Some protocol features that are not very useful in most use-cases were omitted. 
 
 [1] and [2] can be downloaded from here: https://www.usb.org/sites/default/files/USBTMC_1_006a.zip.
 
-The functionality of this package is implemented in the UsbTmcInterface class, which we import here so it will be
-available for import directly from the usbtmc package.
+The functionality of this package is implemented in the UsbTmcInterface class, which we import here to
+make it available for import directly from the usbtmc package.
 """
 
 from .usbtmc_interface import UsbTmcInterface
